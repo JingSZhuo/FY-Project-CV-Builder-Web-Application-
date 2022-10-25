f7796c0f82c7aab5c4b047ea8a4fa18c37803dc4
JingSZhuo <jing2001@live.co.uk>
2022-10-26 00:14:07 +0100

    added customise page

--- cv-builder/src/main.js
+++ cv-builder/src/main.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './main.css';
+import './reset.css';
 
 function MainApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <nav className='navbar'>
+
+      </nav>
+
+
+      <section className='section1'>
+        <h1 className='header'>Header h1</h1>
+      </section>
     </div>
   );
 }
---
+++ cv-builder/src/pages/customisePage.js
@@ -0,0 +1,15 @@
+import '../main.css';
+import '../reset.css';
+
+function CustomiseApp () {
+    return(
+        <div className='main'>
+            <section className='section1'>
+                <h1>Customise page</h1>
+            </section>
+
+        </div>
+    )
+}
+
+export default CustomiseApp;
