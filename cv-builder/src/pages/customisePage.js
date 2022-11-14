278770fc4afaec0bc2884f0d84af041fbcd4ec0d
JingSZhuo <jing2001@live.co.uk>
2022-11-14 23:47:55 +0000

    implemented creating an element and saving it to DB

--- cv-builder/src/pages/customisePage.js
+++ cv-builder/src/pages/customisePage.js
@@ -6,104 +6,19 @@ import '../reset.css';
 import interact from 'interactjs'; 
 
 /*FIREBASE*/
-import { get, getDatabase, onValue, ref, set, update } from 'firebase/database';
+import { getDatabase, onValue, ref, update } from 'firebase/database';
 import db from '../firebase';
 import { useEffect, useState } from 'react';
 
 
 function CustomiseApp () {
 
-    //STATES
+    //STATES + USEEFFECT
 
     const [data, setData] = useState([])
+    const [count, setCount] = useState(0)
 
-    // target elements with the "draggable" class
-    interact('.draggable')
-        .draggable({
-        // enable inertial throwing
-        inertia: true,
-        // keep the element within the area of it's parent
-        modifiers: [
-            interact.modifiers.restrictRect({
-            restriction: 'parent',
-            endOnly: true
-            })
-        ],
-        // enable autoScroll
-        autoScroll: true,
-
-        listeners: {
-            // call this function on every dragmove event
-            
-            move: dragMoveListener,
-
-            // call this function on every dragend event
-            end (event) {
-            var textEl = event.target.querySelector('p')
-
-            textEl && (textEl.textContent =
-                'moved a distance of ' +
-                (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                        Math.pow(event.pageY - event.y0, 2) | 0))
-                .toFixed(2) + 'px')
-
-            GetClass(db)  //Update every move
-
-            }
-        }
-    })
-
-    function dragMoveListener (event) {
-
-        let target = event.target
-        // keep the dragged position in the data-x/data-y attributes
-        let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-        let y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-        // translate the element
-        target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-        // update the posiion attributes
-        target.setAttribute('data-x', x)
-        target.setAttribute('data-y', y)
-    }
-
-    // this function is used later in the resizing and gesture demos
-    window.dragMoveListener = dragMoveListener
-
-    function GetClass (db) {
-        let x = document.getElementsByClassName("draggable").length
-        console.log("lenght: ",x)
-        const object = {}
-        for (let index = 0; index < x; index++) {
-            let y = document.getElementsByClassName("draggable")[index].outerHTML
-            object[index] =  y
-        }
-        //console.log(object)
-
-        let z = getDatabase(db)
-        update(ref(z, 'user/object'), object)
-
-        return object
-    }
-
-    function testFunction(db) {
-
-        console.log("function success")
-
-        const data = getDatabase(db)
-        set(ref( data, 'user/'), {
-            username: "lol",
-            outerDiv: "some...div...changed",
-            empty: "" ,
-            object : {
-                0: "reset...",
-                1: "reset...", 
-                2: "reset...",
-            },
-        });
-    }
-
+    
     useEffect(() => {                               //When page is initially loaded/refreshed
         function readData (db) {
             const data = getDatabase(db)
@@ -112,26 +27,141 @@ function CustomiseApp () {
             onValue(reference, (snapshot) => {
                 const B = JSON.stringify(snapshot.val())
                 const A = JSON.parse(B)
-                console.log(A)
-                setData(A)
+                console.log("A:",A)
+                setData(A)         //Update state data
+                setCount(A.length)
             } , 
             {onlyOnce: true})
         }
         readData(db)
-    }, [])
 
-    let i = 0
-    console.log("Data: ", data[i])
+        
+        loadElements()
+    }, [count]) //count dependency to render the page to load elements
 
-    function createElement() {
+    //JavaScript Library for drag and resize function
+
+    interact('.resize-drag')
+        .resizable({
+            // resize from all edges and corners
+            edges: { left: true, right: true, bottom: true, top: true },
+        
+            listeners: {
+                move (event) {
+                        var target = event.target
+                        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+                        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+                
+                        // update the element's style
+                        target.style.width = event.rect.width + 'px'
+                        target.style.height = event.rect.height + 'px'
+                
+                        // translate when resizing from top or left edges
+                        x += event.deltaRect.left
+                        y += event.deltaRect.top
+                
+                        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+                
+                        target.setAttribute('data-x', x)
+                        target.setAttribute('data-y', y)
+                        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+
+                        UpdateRealTimeDB(db)                //update DB, when a new element is created, this function is also called
+                    },
+                dragMoveListener,
+                },
+            modifiers: [
+            // keep the edges inside the parent
+            interact.modifiers.restrictEdges({
+                outer: 'parent'
+            }),
+        
+            // minimum size
+            interact.modifiers.restrictSize({
+                min: { width: 100, height: 50 }
+            })
+            ],
+        })
+        .draggable({
+            listeners: { 
+                move: window.dragMoveListener,  
+
+                end (event) {
+                    UpdateRealTimeDB(db)        //Update the DB everytime a action is performed
+                }
+                    
+            },
+            inertia: true,
+            modifiers: [
+              interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+              })
+            ]
+        })
+
+    function dragMoveListener (event) {
+            var target = event.target
+            // keep the dragged position in the data-x/data-y attributes
+            var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+            var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+            
+            // translate the element
+            target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+            
+            // update the posiion attributes
+            target.setAttribute('data-x', x)
+            target.setAttribute('data-y', y)
+
+            UpdateRealTimeDB(db)        //Update the DB everytime a action is performed
+        }
+        
+    // this function is used later in the resizing and gesture demos
+    window.dragMoveListener = dragMoveListener
+
+
+        
+
+    function UpdateRealTimeDB (db) {                //Call this everytime to update the Database
+        let x = document.getElementsByClassName("resize-drag").length
+       // console.log("length: ",x)
+        const object = {}                                        //Object created
+        for (let index = 0; index < x; index++) {
+            let y = document.getElementsByClassName("resize-drag")[index].outerHTML     
+            object[index] = y                                   //objectName["property name"] = property value
+        }
+
+        //update database
+        let dbReference = getDatabase(db)
+        update(ref(dbReference, 'user/object'), object)
+
+        return object
+    }
+
+    function loadElements() {       //When called, it will load data from database via the data usestate
+        let len = count
+        for (let index = 0; index < len; index++) {
+            const divv = document.createElement("div")
+
+            const parent = document.getElementById('maindiv')
+            parent.appendChild(divv)
+            divv.outerHTML = data[index]
+        }
+        console.log("len", count)
+    }
+
+    function createElement() {                //Write into database a new element 
         const divv = document.createElement("div")
+
+        const defaultInnerDiv = '<div class="resize-drag" style="transform: translate(35.7335px, 41.8653px); width: 150px; height: 99.9688px; cursor: move;" data-x="35.733471313331904" data-y="41.86525782767984">150×100</div>'
 
         const parent = document.getElementById('maindiv')
         parent.appendChild(divv)
-        divv.outerHTML = data[i]
+        divv.outerHTML = defaultInnerDiv
+
+        
 
         console.log(divv.outerHTML)
-
     }
 
     
@@ -140,28 +170,10 @@ function CustomiseApp () {
             <section id='maindiv' className='section1'>
                 <h1>Customise page</h1>
 
-                {/* <div  class="draggable">
-                    <p> ...</p>
-                </div>
-                <div class="draggable">
-                    <p> ...</p>
-                </div> */}
-
-                {data[i]}
-
-                {
-
-                }
-{/* 
-                {
-                    data.map((data) => {
-                        {data.empty}
-                    })
-                } */}
-
                 {/* <button onClick={() => { testFunction(db)}}>Reset</button> */}
-                <button onClick={() => { GetClass(db)}}>Change</button>
-                <button onClick={() => { createElement()}}>Load</button>
+                {/* <button onClick={() => { UpdateRealTimeDB(db)}}>Create</button> */}
+                <button onClick={() => { createElement()}}>Create</button>
+                {/* <button onClick={() => { loadElements()}}>Load</button> */}
 
             </section>
 
